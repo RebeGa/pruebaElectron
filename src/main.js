0b4187ea90ad7782a20cff8788e050a2acf08b5c
RebeGa <rebeca.perez.garcia@gmail.com>
2025-10-03 12:31:04 -0600

    cambios de navbar y funcion

--- src/main.js
+++ src/main.js
@@ -8,7 +8,45 @@ const createWindow = () => {
     window.loadFile('src/index.html')
 }
 
+const createcrearFormulario = () => {
+    const window = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    window.loadFile('src/crearFormulario.html')
+}
+
+const creategenerarEjericicio = () => {
+      const window = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    window.loadFile('src/generarEjercicio.html')
+
+}
+
+const createeliminarFormulario = () => {
+    const window = new BrowserWindow ({
+        width: 800,
+        height: 600
+    })
+
+    window.loadFile('src/eliminarFormulario.html')
+}
+
+const createagregarFormulario = () => {
+    const window = new BrowserWindow ({
+        width: 800,
+        height: 600
+    })
+    window.loadFile('src/agregarFormulario.html')
+}
+
 
 app.whenReady().then(() =>{
     createWindow()
+    createcrearFormulario()
+    creategenerarEjericicio()
+    createeliminarFormulario()
+    createagregarFormulario()
 })
--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,25 @@
 const $ = selector => document.querySelector(selector)
+const tabs = document.querySelectorAll('.tab-button');
+const sheets = document.querySelectorAll('.sheet-content');
 
 const $count = $('#count')
 const $button = $('button')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    // Desactivar pestañas y hojas activas
+    tabs.forEach(t => t.classList.remove('active'));
+    sheets.forEach(s => s.classList.remove('active'));
+
+    // Activar la pestaña y hoja seleccionadas
+    tab.classList.add('active');
+    const targetId = tab.getAttribute('data-target');
+    document.getElementById(targetId).classList.add('active');
+  });
+});
 
 $button.addEventListener('click',() => {
     const count = +$count.innerHTML
     $count.innerHTML = (count + 1).toString()
 })
+
